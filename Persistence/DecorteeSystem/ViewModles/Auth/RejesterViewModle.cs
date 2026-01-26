393878da403133719fbffda8d6453b5145071118
nada abdelmonem <nadaabdelmonem953@gmail.com>
2026-01-26 15:10:00 +0200

    ...........

--- Persistence/DecorteeSystem/ViewModles/Auth/RejesterViewModle.cs
+++ Persistence/DecorteeSystem/ViewModles/Auth/RejesterViewModle.cs
@@ -1,5 +1,5 @@
 ﻿namespace DecorteeSystem.ViewModles.Auth
 {
-    public record RejesterViewModle(string Name, string Password, string Email);
+    public record RejesterViewModle(string Name,  string Email ,string Password);
 
 }
